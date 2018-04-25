525d03bb3b59860eb6411653303e8c8a673bfcf6
Dimitris Mylonas <gnout@hotmail.com>
2018-04-25 10:25:02 +0300

    Replace OnElemenetChanged with ViewDidLoad

--- CustomerApp/CustomerApp.iOS/ScanPassportPageRenderer.cs
+++ CustomerApp/CustomerApp.iOS/ScanPassportPageRenderer.cs
@@ -13,67 +13,67 @@ namespace CustomerApp.iOS
 {
     public class ScanPassportPageRenderer : PageRenderer, IAnylineMRZModuleDelegate
     {
-        private ScanPassportPage _scanPassportPage;
-        private AnylineMRZModuleView _scanView;
-        private CGRect _frame;
-        private NSError _error;
         private UIAlertView _alert;
+        AnylineMRZModuleView _anylineMrzView;
+        NSError _error;
+        bool _success;
+        public UIAlertView Alert { get; private set; }
 
-        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        private string _licenseKey = "eyJzY29wZSI6WyJBTEwiXSwicGxhdGZvcm0iOlsiaU9TIiwiQW5kcm9pZCIsIldpbmRvd3MiXSwidmFsaWQiOiIyMDE4LTA1LTIwIiwibWFqb3JWZXJzaW9uIjoiMyIsImlzQ29tbWVyY2lhbCI6ZmFsc2UsInRvbGVyYW5jZURheXMiOjMwLCJzaG93UG9wVXBBZnRlckV4cGlyeSI6dHJ1ZSwiaW9zSWRlbnRpZmllciI6WyJjb20uc3RhbXAuY3VzdG9tZXJhcHAiXSwiYW5kcm9pZElkZW50aWZpZXIiOlsiY29tLnN0YW1wLmN1c3RvbWVyYXBwIl0sIndpbmRvd3NJZGVudGlmaWVyIjpbImNvbS5zdGFtcC5jdXN0b21lcmFwcCJdfQpPZS9iTng3dk5GN2U1aEVTcTZ4L0d0MU5iaVpjWGo5M0haS3FWQ3dzdHJXTkErcDdHdlg0ajh5MzNIRGRnRmRjSng3MVl3NHlvR0N6SlRoWU5TbEJ4L2lYVFJoRWI1d0k2UGNYaXpwVU43eHZhL0JUUjlJc3NEdnlZWDRTR1ZZdCtHMm9KWFhXdUttRXVFV3ZNMHNGUWo4WHZwbHFZdW5vV2gyelZMcmpibEVEVVdzajd6bmNWUHpML0NOZGs0Y2M5emNPY09Hdjc1Y0pGUDdyeFc5cG42bkE1eTFnOU91OEIvTkcxNHVkNG9wSFVRQ056aEsrT3pZVk9rOEQ4TE51aEV5SXVFandTS2ZiNkQ3eFBTNXYxUFoxWitRcCtRY3ZGNnIzbmd1VGozUHVMRzlaa2I4ZytORDMzNDRRZ0RsazZ0TTYzTjg5THVhWDNJN0lEUkUxTnc9PQ==";
+
+        public override void ViewDidLoad()
         {
-            base.OnElementChanged(e);
+            base.ViewDidLoad();
 
-            if (e.OldElement != null || Element == null)
+            CGRect frame = UIScreen.MainScreen.ApplicationFrame;
+            frame = new CGRect(frame.X,
+                frame.Y - 20,
+                frame.Width,
+                frame.Height);
+
+            _anylineMrzView = new AnylineMRZModuleView(frame);
+
+            _error = null;
+            // We tell the module to bootstrap itself with the license key and delegate. The delegate will later get called
+            // by the module once we start receiving results.
+
+            _success = _anylineMrzView.SetupWithLicenseKey(_licenseKey, Self, out _error);
+            // SetupWithLicenseKey:delegate:error returns true if everything went fine. In the case something wrong
+            // we have to check the error object for the error message.
+            if (!_success)
             {
-                return;
+                // Something went wrong. The error object contains the error description
+                (Alert = new UIAlertView("Error", _error.DebugDescription, (IUIAlertViewDelegate)null, "OK", null)).Show();
             }
 
-            _scanPassportPage = (ScanPassportPage)Element;
+            // After setup is complete we add the module to the view of this view controller
+            View.AddSubview(_anylineMrzView);
 
-            try
-            {
-                var licenseKey = _scanPassportPage.LicenseKey;
+            //we'll manually cancel scanning
+            _anylineMrzView.CancelOnResult = true;
 
-                // Initializing the view.
-                _frame = UIScreen.MainScreen.ApplicationFrame;
+            //set strict mode here
+            _anylineMrzView.StrictMode = true;
 
-                _frame = new CGRect(_frame.X,
-                    _frame.Y - 20,
-                    _frame.Width,
-                    _frame.Height);
+            StartScanning();
 
-                _scanView = new AnylineMRZModuleView(_frame);
+            /*
+             ALIdentificationView will present the scanned values. Here we start listening for taps
+             to later dismiss the view.
+             */
+            //_idView = new AnylineIdentificationView(new CGRect(0, 0, 300, 300 / 1.4f));
+            //_idView.AddGestureRecognizer(new UITapGestureRecognizer(this, new ObjCRuntime.Selector("ViewTapSelector:")));
 
-                // Initializing the module.
-                _error = null;
-                var success = _scanView.SetupWithLicenseKey(licenseKey, Self, out _error);
+            //_idView.Center = View.Center;
+            //_idView.Alpha = 0;
 
-                if (!success)
-                {
-                    (_alert = new UIAlertView("Error", _error.DebugDescription, (IUIAlertViewDelegate)null, "OK", null)).Show();
-                }
+            //View.AddSubview(_idView);
 
-                _scanView.CancelOnResult = true;
-
-                // After setup, add the scanView to the view
-                View.AddSubview(_scanView);
-
-                _error = null;
-                if (_error != null)
-                    (_alert = new UIAlertView("Error", _error.DebugDescription, (IUIAlertViewDelegate)null, "OK", null)).Show();
-
-                // Start scanning
-                StartScanning();
-            }
-            catch (Exception exception)
-            {
-                (_alert = new UIAlertView("Error", "Unexpected Error Occured", (IUIAlertViewDelegate)null, "OK", null)).Show();
-            }
         }
 
         void StartScanning()
         {
-            var success = _scanView.StartScanningAndReturnError(out _error);
+            var success = _anylineMrzView.StartScanningAndReturnError(out _error);
 
             if (!success)
                 (_alert = new UIAlertView("Error", _error.DebugDescription, (IUIAlertViewDelegate)null, "OK", null)).Show();
