bf9a1a38fc68c7985f3b8628216d9de989554de2
Dimitris Mylonas <gnout@hotmail.com>
2018-05-08 15:34:17 +0300

    Changes

--- CustomerApp/CustomerApp.iOS/ScanPassportPageRenderer.cs
+++ CustomerApp/CustomerApp.iOS/ScanPassportPageRenderer.cs
@@ -17,10 +17,14 @@ namespace CustomerApp.iOS
         AnylineMRZModuleView _anylineMrzView;
         NSError _error;
         bool _success;
+
         public UIAlertView Alert { get; private set; }
 
+        // temp license
         private string _licenseKey = "eyJzY29wZSI6WyJBTEwiXSwicGxhdGZvcm0iOlsiaU9TIiwiQW5kcm9pZCIsIldpbmRvd3MiXSwidmFsaWQiOiIyMDE4LTA1LTIwIiwibWFqb3JWZXJzaW9uIjoiMyIsImlzQ29tbWVyY2lhbCI6ZmFsc2UsInRvbGVyYW5jZURheXMiOjMwLCJzaG93UG9wVXBBZnRlckV4cGlyeSI6dHJ1ZSwiaW9zSWRlbnRpZmllciI6WyJjb20uc3RhbXAuY3VzdG9tZXJhcHAiXSwiYW5kcm9pZElkZW50aWZpZXIiOlsiY29tLnN0YW1wLmN1c3RvbWVyYXBwIl0sIndpbmRvd3NJZGVudGlmaWVyIjpbImNvbS5zdGFtcC5jdXN0b21lcmFwcCJdfQpPZS9iTng3dk5GN2U1aEVTcTZ4L0d0MU5iaVpjWGo5M0haS3FWQ3dzdHJXTkErcDdHdlg0ajh5MzNIRGRnRmRjSng3MVl3NHlvR0N6SlRoWU5TbEJ4L2lYVFJoRWI1d0k2UGNYaXpwVU43eHZhL0JUUjlJc3NEdnlZWDRTR1ZZdCtHMm9KWFhXdUttRXVFV3ZNMHNGUWo4WHZwbHFZdW5vV2gyelZMcmpibEVEVVdzajd6bmNWUHpML0NOZGs0Y2M5emNPY09Hdjc1Y0pGUDdyeFc5cG42bkE1eTFnOU91OEIvTkcxNHVkNG9wSFVRQ056aEsrT3pZVk9rOEQ4TE51aEV5SXVFandTS2ZiNkQ3eFBTNXYxUFoxWitRcCtRY3ZGNnIzbmd1VGozUHVMRzlaa2I4ZytORDMzNDRRZ0RsazZ0TTYzTjg5THVhWDNJN0lEUkUxTnc9PQ==";
 
+
+        // Version 1 without Async
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
@@ -34,12 +38,9 @@ namespace CustomerApp.iOS
             _anylineMrzView = new AnylineMRZModuleView(frame);
 
             _error = null;
-            // We tell the module to bootstrap itself with the license key and delegate. The delegate will later get called
-            // by the module once we start receiving results.
 
             _success = _anylineMrzView.SetupWithLicenseKey(_licenseKey, Self, out _error);
-            // SetupWithLicenseKey:delegate:error returns true if everything went fine. In the case something wrong
-            // we have to check the error object for the error message.
+
             if (!_success)
             {
                 // Something went wrong. The error object contains the error description
@@ -56,20 +57,51 @@ namespace CustomerApp.iOS
             _anylineMrzView.StrictMode = true;
 
             StartScanning();
-
-            /*
-             ALIdentificationView will present the scanned values. Here we start listening for taps
-             to later dismiss the view.
-             */
-            //_idView = new AnylineIdentificationView(new CGRect(0, 0, 300, 300 / 1.4f));
-            //_idView.AddGestureRecognizer(new UITapGestureRecognizer(this, new ObjCRuntime.Selector("ViewTapSelector:")));
-
-            //_idView.Center = View.Center;
-            //_idView.Alpha = 0;
-
-            //View.AddSubview(_idView);
-
         }
+
+        //Version 2 Async that doesn't scan
+        //public override void ViewDidLoad()
+        //{
+        //    base.ViewDidLoad();
+
+        //    CGRect frame = UIScreen.MainScreen.ApplicationFrame;
+        //    frame = new CGRect(frame.X,
+        //        frame.Y - 20,
+        //        frame.Width,
+        //        frame.Height);
+
+        //    _anylineMrzView = new AnylineMRZModuleView(frame);
+
+        //    // After setup is complete we add the module to the view of this view controller
+        //    View.AddSubview(_anylineMrzView);
+
+        //    _anylineMrzView.SetupAsyncWithLicenseKey(_licenseKey, Self, (b, error) =>
+        //    {
+        //        StartScanning();
+        //    });
+        //}
+
+        // Version 3 Async that doesn't work
+        //public override void ViewDidLoad()
+        //{
+        //    base.ViewDidLoad();
+
+        //    CGRect frame = UIScreen.MainScreen.ApplicationFrame;
+        //    frame = new CGRect(frame.X,
+        //        frame.Y - 20,
+        //        frame.Width,
+        //        frame.Height);
+
+        //    _anylineMrzView = new AnylineMRZModuleView(frame);
+
+        //    _anylineMrzView.SetupAsyncWithLicenseKey(_licenseKey, Self, (b, error) =>
+        //    {
+        //        // After setup is complete we add the module to the view of this view controller
+        //        View.AddSubview(_anylineMrzView);
+
+        //        StartScanning();
+        //    });
+        //}
 
         void StartScanning()
         {
@@ -88,7 +120,7 @@ namespace CustomerApp.iOS
                 (_alert = new UIAlertView("Passport", $"The passport id: {passportData.DocumentNumber}", (IUIAlertViewDelegate)null, "OK", null)).Show();
             }
 
-            //NavigationController.PopViewController(true);
+            NavigationController.PopViewController(true);
         }
     }
 }
